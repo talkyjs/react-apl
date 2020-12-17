208423dfd5bb5678aafd1a5bd7b8c8dae8612d8e
Hidetaka Okamoto <info@wp-kyoto.net>
2020-12-17 22:00:37 +0900

    chore: some updates

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -5,7 +5,29 @@ import { Default as Thing } from '../stories/Thing.stories';
 describe('Thing', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    ReactDOM.render(<Thing aplDocument={{
+      "type": "APL",
+      "version": "1.4",
+      "theme": "dark",
+      "mainTemplate": {
+        "parameters": [],
+        "items": [
+          {
+            "type": "Container",
+            "alignItems": "center",
+            "justifyContent": "spaceAround",
+            "items": [
+              {
+                "type": "Text",
+                "text": "Hello",
+                "fontSize": "50px",
+                "color": "rgb(251,184,41)"
+              }
+            ]
+          }
+        ]
+      }
+    }} />, div);
+    //ReactDOM.unmountComponentAtNode(div);
   });
 });
